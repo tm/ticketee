ab03383cc89f5d92824089caa27af06952c77b6f
Tomasz Mieloch <tomasz@mieloch.net>
2011-12-08 17:07:09 +0100

    Added rate limiting to the V1 API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111206133028) do
+ActiveRecord::Schema.define(:version => 20111208152651) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(:version => 20111206133028) do
     t.datetime "updated_at"
     t.boolean  "admin",                                 :default => false
     t.string   "authentication_token"
+    t.integer  "request_count",                         :default => 0
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
